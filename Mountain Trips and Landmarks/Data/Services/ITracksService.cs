66770daded390ddd9ab2ac459b793f6ae037516b
Peter Matov <pmatov@gmail.com>
2021-12-10 01:17:59 +0200

    ITrackService commit

---
+++ Mountain Trips and Landmarks/Data/Services/ITracksService.cs
@@ -0,0 +1,14 @@
+﻿using Mountain_Trips_and_Landmarks.Data.Base;
+using Mountain_Trips_and_Landmarks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mountain_Trips_and_Landmarks.Data.Services
+{
+    public interface ITracksService: IEntityBaseRepository<Track>
+    {
+        Task<Track> GetTrackByIdAsync(int id);
+    }
+}
